535c46596fc9e666767b57a8313f6aeb6610e918
DuyetVu2001 <64247057+DuyetVu2001@users.noreply.github.com>
2021-10-10 10:03:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const express = require('express');
 const cors = require('cors');
+// const mongoose = require('mongoose');
 
 const route = require('./routes');
 
 const app = express();
 const port = 4000;
 
+// MongoDB
+// const run = async () => {
+// 	try {
+// 		await mongoose.connect(
+// 			'mongodb+srv://youtube:youtube@youtube.jug43.mongodb.net/youtube-data?retryWrites=true&w=majority'
+// 		);
+// 		console.log('Connect to mongoDB successfully!');
+// 		app.listen(port, () => {
+// 			console.log('Server is running on port: ', port);
+// 		});
+// 	} catch (error) {
+// 		console.error('MongoDB error: ', error);
+// 	}
+// };
+
 // Middleware
 app.use(express.json());
 app.use(cors());
 route(app);
 
-app.listen(port, () => {
-	console.log('Server is running on port: ', port);
-});
+// Run
+// run();
